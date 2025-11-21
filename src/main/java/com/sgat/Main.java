f76f1a11573d1286fb2f2aec7760a083c90ecc2d
lucaasiFx <lucaasifx@gmail.com>
2025-11-21 01:10:10 -0300

    :sparkles: feat: Conexão com o banco de dados

--- src/main/java/com/sgat/Main.java
+++ src/main/java/com/sgat/Main.java
@@ -5,10 +5,17 @@ import com.sgat.view.MainLayout;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import java.util.Objects;
 
 public class Main extends Application {
+    // por enquanto a conexão está hardcoded, favor colocar as suas credenciais
+    static final String DB_URL = "jdbc:postgresql://localhost:5432/sgat";
+    static final String DB_USER = "postgres";
+    static final String DB_PASS = "123";
 
     @Override
     public void start(Stage primaryStage) {
@@ -27,6 +34,13 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        try {
+            DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+            System.out.println("FUNFOU");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         launch(args);
+
     }
 }
