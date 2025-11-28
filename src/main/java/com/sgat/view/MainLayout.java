3227206462f03e724e0d6fc1654386e4a1ba5c40
Francisco Sérgio Feitosa <fsfsegundo@hotmail.com>
2025-11-28 01:58:29 -0300

    ahahahah

--- src/main/java/com/sgat/view/MainLayout.java
+++ src/main/java/com/sgat/view/MainLayout.java
@@ -124,11 +124,26 @@ public class MainLayout {
     private Node createSidebarHeader() {
         HBox header = new HBox();
         header.getStyleClass().add("sidebar-header");
+
+        StackPane iconContainer = new StackPane();
+        iconContainer.getStyleClass().add("sidebar-header-icon-container");
+        SVGPath planeIcon = new SVGPath();
+        planeIcon.setContent("M17.8 19.2 16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z");
+        planeIcon.getStyleClass().add("icon-svg");
+        iconContainer.getChildren().add(planeIcon);
+
+        VBox titleBox = new VBox(-2);
+        titleBox.setAlignment(Pos.CENTER_LEFT);
         Label title = new Label("TravelManager");
         title.getStyleClass().add("sidebar-header-title");
-        title.visibleProperty().bind(sidebarCollapsed.not());
-        title.managedProperty().bind(sidebarCollapsed.not());
-        header.getChildren().add(title);
+        Label subtitle = new Label("Sistema de Gestão");
+        subtitle.getStyleClass().add("sidebar-header-subtitle");
+        titleBox.getChildren().addAll(title, subtitle);
+
+        titleBox.visibleProperty().bind(sidebarCollapsed.not());
+        titleBox.managedProperty().bind(sidebarCollapsed.not());
+
+        header.getChildren().addAll(iconContainer, titleBox);
         return header;
     }
 
